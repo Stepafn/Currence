adf4890d2afa975e61308f2efc522448e8106e64
Stepafn <steeva409@gmail.com>
2023-07-27 11:41:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,71 +1,74 @@
-import configargparse  # To run a Python script using additional arguments on the command line and passing variables
-import asyncio  # allows you to do asynchronous programming
-import requests  # URL processing module
-from bs4 import BeautifulSoup  # Module for working with HTML
-import logging  # allows you to write logs to a file:
+import configargparse  # imports the configargparse module, which allows you to specify program options in the configuration file.
+import asyncio  # imports the asyncio module for asynchronous programming.
+import requests  # imports the requests module to send HTTP requests.
+from bs4 import BeautifulSoup  # imports the BeautifulSoup class from the bs4 module for parsing HTML.
+import logging  # imports the logging module for logging.
 
 
-class ParseIni:
-
-    def __init__(self):
-        parser = configargparse.ArgParser()
+class ParseIni:  # defines the ParseIni class.
+    def __init__(self):  # class constructor.
+        parser = configargparse.ArgParser()  # creates a command line argument parser object.
         parser.add_argument('-c', '--config', required=True, is_config_file=True, help='Path to file config.ini')
-        parser.add_argument('--dollar_rub', help='ruble exchange rate')
-        parser.add_argument('--sleep', help='Пауза')
-        parser.add_argument('--tracking_point', help='change rate point')
-        args = parser.parse_args()
-        self.dollar_rub = args.dollar_rub
+        parser.add_argument('--currency_source', default='http://www.finmarket.ru/currency/USD/',
+                            help='Currency web-source')
+        parser.add_argument('--sleep', default=3, type=int, help='Update delay in seconds')
+        parser.add_argument('--tracking_point', default=0.5, type=float, help='Change rate point')
+        parser.add_argument('--headers', default={'User-Agent': 'Mozilla/5.0'}, help='Change rate point')
+        parser.add_argument('--log_config', default={"level": logging.INFO,
+                                                     "format": '%(asctime)s %(levelname)s %(message)s'},
+                            help='Log configs')  # adds arguments that can be passed on the command line.
+        args = parser.parse_args()  # parses the passed arguments and stores them in args.
+        self.currency_source = args.currency_source
         self.sleep = args.sleep
         self.tracking_point = args.tracking_point
+        self.headers = args.headers
+        self.log_config = args.log_config
 
 
-# Main class
-class Currency:
-    """Link to the desired page"""
-    parse = ParseIni()
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'}
+class Currency:  # defines the Currency class.
+    used_args = ParseIni()  # creates an instance of the ParseIni class and stores it in used_args.
 
-    def __init__(self):
+    def __init__(self):  # class constructor.
         self.loop = asyncio.get_event_loop()
+        self.previous_currency = None
 
-    async def get_currency_price(self):
-        """Method for getting the exchange rate"""
-        # Parse the entire page
-        full_page = await self.loop.run_in_executor(None, requests.get, self.parse.dollar_rub,
-                                                    {'headers': self.headers})
-        # Parsing through BeautifulSoup
+    async def get_currency_price(self):  # asynchronous method for getting the exchange rate.
+        full_page = await self.loop.run_in_executor(None, requests.get, self.used_args.currency_source,
+                                                    headers=self.used_args.headers)
+
         soup = BeautifulSoup(full_page.content, 'html.parser')
-
-        # Get the value we need and return it
         convert = soup.findAll("div", {"class": "valvalue"})
         return convert[0].text
 
-    async def check_currency(self):
-        """Check currency change"""
-        logging.basicConfig(filename='log.txt', level=logging.INFO,
-                            format='%(asctime)s %(levelname)s %(message)s')
+    async def check_currency(self):  # an asynchronous method for tracking course changes.
+        logging.basicConfig(filename='log.txt', **self.used_args.log_config)
         while True:
             currency = await self.get_currency_price()
             currency = float(currency.replace(",", "."))
-            if currency >= currency + float(self.parse.tracking_point):
-                logging.info("The course has grown a lot!")
-            elif currency <= currency - float(self.parse.tracking_point):
-                logging.info("The course has dropped a lot!")
-            logging.info(f"Current rate: 1 dollar = {str(currency)}")
-            await asyncio.sleep(int(self.parse.sleep))
+            if self.previous_currency is not None:
+                if currency >= self.previous_currency + self.used_args.tracking_point:
+                    logging.info("The course has grown a lot!")
+                    print(f"The course has grown a lot! {currency}")
+                elif currency <= self.previous_currency - self.used_args.tracking_point:
+                    logging.info("The course has dropped a lot!")
+                    print(f"The course has dropped a lot! {currency}")
+            self.previous_currency = currency
+            logging.info(f"Current rate: 1 dollar = {currency}")
+            print(f"Current rate: 1 dollar = {currency}")
+            await asyncio.sleep(self.used_args.sleep)
 
 
-async def main():
+async def main():  # asynchronous main function.
     while True:
         try:
-            start: str = input("Enter command: ")
+            start = input("Enter command: ")
             if start == "Currency":
                 await Currency().check_currency()
-            raise ValueError
+            else:
+                raise ValueError
         except ValueError:
             print("Error! Try again?")
 
 
-if __name__ == "__main__":
-    asyncio.run(main())
+if __name__ == "__main__":  # checks if the script is run directly and not imported as a module.
+    asyncio.run(main())  # starts an asynchronous loop with a call to the main() function.
