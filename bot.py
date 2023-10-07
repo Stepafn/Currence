09ae14ea96225bcd3b78b33db283c42b31b92ba3
Stepafn <138586684+Stepafn@users.noreply.github.com>
2023-10-07 13:58:38 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,27 @@
 import asyncio
+import logging
 from aiogram import Bot, Dispatcher, F
 from aiogram.types import Message
+import configparser
 
-bot = Bot(token='6500524410:AAElwMSMO8zD72UHtqRh9OBPJnh4LCifkc8')
+logging.basicConfig(level=logging.INFO)
+
+config = configparser.ConfigParser()
+config.read("config.txt")
+bot_token = config.get('bot', 'token')
+
+bot = Bot(token=bot_token)
 dp = Dispatcher()
 
 
 @dp.message(F.text == '/start')
 async def cmd_start(message: Message):
+    logging.info('Received /start command')
     await message.answer('Hi! I am a bot that can add 1 + 1')
 
 
 @dp.message(F.text == '1+1')
-async def cmd_1(message: Message):
+async def cmd_answer(message: Message):
     await message.answer('Answer 3')
 
 
@@ -27,3 +36,4 @@ async def main():
 
 if __name__ == '__main__':
     asyncio.run(main())
+
