3d34058b18ae7adb693df478c506029933d8dd5d
Stepafn <138586684+Stepafn@users.noreply.github.com>
2023-10-18 21:03:52 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,39 +1,39 @@
 import asyncio
-import logging
 from aiogram import Bot, Dispatcher, F
 from aiogram.types import Message
-import configparser
+from parsers import ParseIni
+from logger import get_logger
 
-logging.basicConfig(level=logging.INFO)
-
-config = configparser.ConfigParser()
-config.read("config.txt")
-bot_token = config.get('bot', 'token')
-
-bot = Bot(token=bot_token)
 dp = Dispatcher()
 
 
 @dp.message(F.text == '/start')
 async def cmd_start(message: Message):
-    logging.info('Received /start command')
+    logger.info('Received /start command')
     await message.answer('Hi! I am a bot that can add 1 + 1')
 
 
 @dp.message(F.text == '1+1')
 async def cmd_answer(message: Message):
+    logger.info('Received "1+1" command')
     await message.answer('Answer 3')
 
 
 @dp.message()
 async def echo(message: Message):
+    logger.info('Received an unknown command')
     await message.answer('Unknown command. I can only add 1 + 1')
 
 
 async def main():
     await dp.start_polling(bot)
 
-
 if __name__ == '__main__':
-    asyncio.run(main())
+    cfg = ParseIni()
+    logger = get_logger(cfg.log_config.get('level'),
+                        cfg.log_config.get('format'),
+                        cfg.log_config.get('filename'))
+    TOKEN = cfg.TOKEN
+    bot = Bot(TOKEN)
 
+    asyncio.run(main())
